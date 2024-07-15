606c63cced340f404c99d39091a0e94a51da79dd
Pritam Das <pritam.das@gspann.com>
2024-07-15 16:54:10 +0530

    donation site changes

--- frontend/src/Pages/Facilities.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Facilities = () => {
-  return <div></div>;
-};
-
-export default Facilities;
---
+++ src/Pages/Facilities.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Facility from "../Components/Facility/Facility";
+
+const Facilities = () => {
+  return (
+    <div>
+      <Facility />
+    </div>
+  );
+};
+
+export default Facilities;
